13cd6be66125c16d71d211fd23d5136269348444
Bart Massey <bart@cs.pdx.edu>
2012-06-02 10:32:33 -0700

    started to revise the script

--- mkgit.sh
+++ mkgit.sh
@@ -8,17 +8,31 @@
 #
 # Loosely based on an earlier script by Julian Kongslie
 
-BIN="`echo $0 | sed 's=[^/]*$=='`"
-SITESCRIPTS="`ls ${BIN} | egrep '^mkgit-'`"
-SITES="`echo $SITESCRIPTS | sed -e 's=mkgit-==g' -e 's= =|='`"
-case $BIN in '') BIN='.' ;; esac
 PGM="`basename $0`"
+
+# The site scripts live in the same bin with mkgit.
+# They are named mkgit-*.
+BIN="`echo $0 | sed 's=/*[^/]*$=='`"
+case $BIN in '') BIN='.' ;; esac
+SITESCRIPTS="`ls ${BIN} | egrep '^mkgit-'`"
+# Pipe-separated list of site names. Used both for
+# display and with shell eval.
+SITES="`echo $SITESCRIPTS | sed -e 's=mkgit-==g' -e 's= =|='`"
+
 USAGE="$PGM: usage: $PGM [-p|-d <desc>] [-X [$SITES|github|na] [<project>[.git]] | ssh://[<user>@]host/<dir>/<project>.git] [<git-dir>]"
 
-X=''
-MKLINK=false
-PUBLIC=false
+# Set by site script: symlinked name for repo.
+REPOLINK=""
+# Repos must be specified as either "public" (will be made
+# visible to all) or "private" (will be made visible only
+# to those with commit access).
 PRIVATE=false
+PUBLIC=false
+# Optional "special" site name that receives
+# custom handling.
+X=""
+
+# HERE
 
 while [ $# -gt 0 ]
 do
@@ -28,23 +42,11 @@ do
 	shift 2
 	;;
     -d)
-	case "$PRIVATE" in
-	true)
-	    echo "$PGM: both private and public were specified"
-	    exit 1
-	    ;;
-	esac
         PUBLIC=true
 	DESC="$2"
 	shift 2
 	;;
     -p)
-	case "$PUBLIC" in
-	true)
-	    echo "$PGM: both public and private were specified"
-	    exit 1
-	    ;;
-	esac
         PRIVATE=true
 	shift
 	;;
@@ -58,13 +60,16 @@ do
     esac
 done
 
-if $PUBLIC || $PRIVATE
-then
-    :
-else
+case $PUBLIC:$PRIVATE in
+true:true)
+    echo "$PGM: both public (-d <desc>) and private (-p) were specified" >&2
+    exit 1
+    ;;
+false:false)
     echo "$PGM: neither public (-d <desc>) nor private (-p) was specified" >&2
     exit 1
-fi
+    ;;
+esac
 
 if [ $# -gt 2 ]
 then
@@ -135,8 +140,10 @@ esac
 
 case $X in
 github)
-    if $PRIVATE
+    if $PUBLIC
     then
+        :
+    else
 	echo "$PGM: cannot create private github repos" >&2
 	exit 1
     fi
@@ -195,7 +202,7 @@ e	echo "failed to set github description:" >&2
     then
       touch git-daemon-export-ok
       echo "${DESC}" >description
-      ${MKLINK} && ln -s "${PARENTQ}/${PROJECTQ}" /git/.
+      [ "${REPOLINK}" != "" ] && ln -s "${PARENTQ}/${PROJECTQ}" "${REPOLINK}"/.
     fi
 EOF
     if [ "$?" -ne 0 ]
